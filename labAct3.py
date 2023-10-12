c4713e6b6d03fb06ae0fc64f33b01430d66b8e58
Jenny Ann Guyong <145944211+jatguyong@users.noreply.github.com>
2023-10-12 22:32:03 +0800

    Add files via upload

---
+++ labAct3.py
@@ -0,0 +1,38 @@
+# Number 1:
+
+num1 = 5
+
+for i in range(1, num1 + 1):
+    for j in range(1, i + 1):
+        print(j, end=' ')
+    print()
+
+print("----------------------------------------")
+
+
+# Number 2:
+
+num_input = int(input("Enter a number: "))
+sum = 0
+
+for i in range(1, num_input + 1):
+    sum += i
+
+print(sum)
+
+
+# Number 3:
+
+print("----------------------------------------")
+
+num2 = 5
+
+for i in range(num2, 0, -1):
+    for j in range(1, i + 1):
+        print(j, end=' ')
+    print()
+
+
+
+
+
